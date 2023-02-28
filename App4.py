089572425213915af9760b3e1a36e674b5c48f1f
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-28 23:22:18 +0500

    Update App4.py

--- App4.py
+++ App4.py
@@ -104,16 +104,16 @@ chart_data = pd.DataFrame(
   columns=['GOLD', 'SILVER', 'BRONZE'])
 st.line_chart(chart_data)
 
-medal_count = athletes_df['Medal'].value_counts()
-st.header('Numbers of Medals Recieved by Each Athletes')
-plt.rcParams['figure.figsize']=[14,8]
-colors = ['green','yellow','blue']
-plt.bar(x=medal_count.index, height=medal_count.values, color=colors)
+#medal_count = athletes_df['Medal'].value_counts()
+#st.header('Numbers of Medals Recieved by Each Athletes')
+#plt.rcParams['figure.figsize']=[14,8]
+#colors = ['green','yellow','blue']
+#plt.bar(x=medal_count.index, height=medal_count.values, color=colors)
 
-plt.title('medal_count')
-plt.xlabel('Medals')
-plt.ylabel('Athletes')
-plt.show()
+#plt.title('medal_count')
+#plt.xlabel('Medals')
+#plt.ylabel('Athletes')
+#plt.show()
 
 
 
