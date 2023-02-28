7516fb96fbef5984bbdf7eb960e710e5126ac425
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-28 23:18:22 +0500

    Update App4.py

--- App4.py
+++ App4.py
@@ -108,7 +108,7 @@ medal_count = athletes_df['Medal'].value_counts()
 st.header('Numbers of Medals Recieved by Each Athletes')
 plt.rcParams['figure.figsize']=[14,8]
 colors = ['green','yellow','blue']
-fig = plt.bar(x=medal_count.index, height=medal_count.values, color=colors)
+fig=plt.bar(x=medal_count.index, height=medal_count.values, color=colors)
 
 plt.title('medal_count')
 plt.xlabel('Medals')
