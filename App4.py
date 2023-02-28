c08583d9c2ea86d5270c4d11b1132dcbb808eadc
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-28 23:33:43 +0500

    Update App4.py

--- App4.py
+++ App4.py
@@ -104,16 +104,16 @@ chart_data = pd.DataFrame(
   columns=['GOLD', 'SILVER', 'BRONZE'])
 st.line_chart(chart_data)
 
-#medal_count = athletes_df['Medal'].value_counts()
-#st.header('Numbers of Medals Recieved by Each Athletes')
-#plt.rcParams['figure.figsize']=[14,8]
-#colors = ['green','yellow','blue']
-#plt.bar(x=medal_count.index, height=medal_count.values, color=colors)
+medal_count = athletes_df['Medal'].value_counts()
+st.header('Numbers of Medals Recieved by Each Athletes')
+plt.rcParams['figure.figsize']=[14,8]
+colors = ['green','yellow','blue']
+fig = plt.bar(x = medal_count.index, height = medal_count.values, color = colors)
 
-#plt.title('medal_count')
-#plt.xlabel('Medals')
-#plt.ylabel('Athletes')
-#plt.show()
+plt.title('medal_count')
+plt.xlabel('Medals')
+plt.ylabel('Athletes')
+st.pyplot()
 
 top_5_sports = athletes_df.groupby('Sport')['Medal'].value_counts().sort_values(ascending=False)
 st.header('Top 5 Medals Recieved in Each Sports')
